57238e0bb7cfadedbc232a2ad3b057be672c96d8
Gabriel <mayfieldgabrielj@gmail.com>
2024-02-26 14:40:09 -0600

    Finishing OOP on to encapsulation

--- adv_py/oop.py
+++ adv_py/oop.py
@@ -6,9 +6,9 @@ class PlayerCharacter:
     # Class Object Attribute
     membership = True
 
-    def __init__(self, name = 'anonymous', age = 0):
+    def __init__(self, name='anonymous', age=0):
         if age > 18:
-            self.name = name # attributes 
+            self.name = name  # attributes
             self.age = age
 
     def shout(self):
@@ -18,11 +18,21 @@ class PlayerCharacter:
     def run(self, hello):
         print(f'I can run fast and my name is {self.name}')
 
+    @classmethod  # class method, can use without instantiating an object.
+    def adding_things(cls, num1, num2):  # use cls instead of self
+        return cls('Teddy', num1 + num2)
+
+    @staticmethod
+    def adding_things2(num1, num2):
+        return num1 + num2
+
 
 player1 = PlayerCharacter('gabe', 23)
-
 print(player1.name)
 print(player1.age)
 print(player1.shout())
 print(player1.membership)
 print(player1.run('hello'))
+print(player1.adding_things(2, 3))
+player3 = PlayerCharacter.adding_things(2, 3) # method on the actual class, doesn't need instantiation.
+print(player3.age)
