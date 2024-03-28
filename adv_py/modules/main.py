9f9fce4aa6f60c2caa43835f7640988926636e63
Gabriel <mayfieldgabrielj@gmail.com>
2024-03-28 12:27:56 -0500

    starting built in modules

---
+++ adv_py/modules/main.py
@@ -0,0 +1,10 @@
+import modules
+import shopping.shopping_cart
+
+print(modules)
+print(f'The quotient of 1 and 2 is: {modules.divide(1,2)}')
+
+print(shopping.shopping_cart.buy('apple'))
+
+
+print(__name__)
---
+++ adv_py/modules/modules.py
@@ -0,0 +1,11 @@
+print(__name__)
+def multiply(num1, num2):
+    return num1+num2
+
+
+def divide(n1, n2):
+    return n1/n2
+
+
+def max():
+    return 'oops'
