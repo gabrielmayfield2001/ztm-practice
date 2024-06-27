c4bc0c0f28f74837894021fc3cffaeca3189f8b5
Gabriel <mayfieldgabrielj@gmail.com>
2024-06-26 22:03:56 -0500

    nothing

--- restful_project/weather_test.py
+++ restful_project/weather_test.py
@@ -10,20 +10,21 @@ complete_url = f"{base_url}appid={api_key}&q={city_name}"
 
 response = requests.get(complete_url)
 
-if response.status_code == 200:
-    print('successful')
-    data = response.json()
+if __name__ == '__main__':
+    if response.status_code == 200:
+        print('successful')
+        data = response.json()
 
-    # Output
+        # Output
 
-    if 'weather' in data and 'main' in data and 'name' in data:
-        print(f'Weather data for the city of {data['name']}')
-        print(f'Condition: {data['weather'][0]['description']}')
-        print(f'Temperature: {data['main']['temp']} Kelvin')
-        print(f'Pressure: {data['main']['pressure']} hPa')
-        print(f'Humidity: {data['main']['humidity']}%')
+        if 'weather' in data and 'main' in data and 'name' in data:
+            print(f'Weather data for the city of {data['name']}')
+            print(f'Condition: {data['weather'][0]['description']}')
+            print(f'Temperature: {data['main']['temp']} Kelvin')
+            print(f'Pressure: {data['main']['pressure']} hPa')
+            print(f'Humidity: {data['main']['humidity']}%')
 
-    else: 
-        print('Some expected data is missing in the response')
-else:
-    print('API request was unsuccessful')
+        else:
+            print('Some expected data is missing in the response')
+    else:
+        print('API request was unsuccessful')
